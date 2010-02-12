a26bc83019e5c2898aedb7acb16ed997f9d30a13
t3hpr1m3 <vmizzle@gmail.com>
2010-02-11 19:58:13 -0600

    Some TDD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,41 @@
 class PostsController < ApplicationController
-  def index
-  end
+	# GET /posts
+	# GEt /posts.xml
+	def index
+		@posts = Post.all
 
-  def show
-  end
+		respond_to do |format|
+			format.html #index.html.erb
+			format.xml { render :xml => @posts }
+		end
+	end
 
-  def new
-  end
+	# GET /posts/1
+	# GET /posts/1.xml
+	def show
+		@post = Post.find( params[:id] )
 
-  def edit
-  end
+		respond_to do |format|
+			format.html # show.html.erb
+			format.xml { render :xml => @post }
+		end
+	end
 
-  def create
-  end
+	# GET /posts/new
+	# GET /post/new.xml
+	def new
+	end
 
-  def update
-  end
+	def edit
+	end
 
-  def delete
-  end
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 
 end
