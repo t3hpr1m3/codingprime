afa2f1e6067eadab5e02a2bc3cafb8d79dd4ec78
t3hpr1m3 <vmizzle@gmail.com>
2010-06-23 08:33:51 -0500

    Added sqlite3 as a dependency.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,3 +30,4 @@ config.action_mailer.delivery_method = :test
   config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
   config.gem "factory_girl", :source => "http://gemcutter.org"
   config.gem "rcov"
+  config.gem "sqlite3-ruby", :lib => 'sqlite3'
