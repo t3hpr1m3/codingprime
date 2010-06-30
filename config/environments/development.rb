d4aa039b5d92fc229a70005d71041c6266496e94
Josh Williams <vmizzle@gmail.com>
2010-06-30 15:48:30 -0500

    Moved development to codingprime.local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,4 +17,4 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = false
 config.gem "sqlite3-ruby", :lib => 'sqlite3'
 config.action_controller.session ||= {}
-config.action_controller.session[:session_domain] = "codingprime.com"
+config.action_controller.session[:session_domain] = "codingprime.local"
