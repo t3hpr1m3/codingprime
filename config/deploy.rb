f3635ac2788019e3c1e4a90fa47b4f72844a0ecb
t3hpr1m3 <vmizzle@gmail.com>
2010-07-29 06:59:21 -0500

    First stab at capistrano.

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+set :application, "codingprime.com"
+set :repository,  "git://github.com/t3hpr1m3/CodingPrime.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_to, "/web/#{application}"
+
+server "foobizzle.com", :app, :web, :db, :primary => true
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
