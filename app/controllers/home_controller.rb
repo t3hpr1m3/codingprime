0a2b7aeb4dcc9739b1bf58920063cbff9b01e0c8
t3hpr1m3 <vmizzle@gmail.com>
2010-08-03 20:43:28 -0500

    Minor styling.  Got rid of the nav for now.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @posts = Post.all
     respond_to do |format|
-      format.html
+      format.html { redirect_to blog_root_url }
       format.atom
     end
   end
