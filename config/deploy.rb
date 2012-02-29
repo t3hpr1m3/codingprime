08d8750c3f575ddc8937869ac362d9659ebc5917
Josh Williams <jwilliams@shareone.com>
2012-02-29 06:30:23 -0600

    Added unicorn gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,19 +8,25 @@ set :repository,  "http://github.com/t3hpr1m3/codingprime.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-set :deploy_to, "/web/#{application}"
+set :deploy_to, "/var/www/#{application}"
 set :user, "titus"
 
 server "foobizzle.com", :app, :web, :db, :primary => true
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
+
+  desc "Start unicorn"
+  task :start, :except => {:no_release => true} do
+    run "cd #{current_path}; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => {:no_release => true} do
+    run "kill -s QUIT `cat #{File.join(current_path, 'tmp', 'pids', 'unicorn.pid')}`"
+  end
+
+  desc "Restart unicorn"
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "kill -s USR2 `cat #{File.join(current_path, 'tmp', 'pids', 'unicorn.pid')}`"
   end
 end
