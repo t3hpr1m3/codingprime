8f26838074e9d58293b38e3b2a52c3bd18f69912
t3hpr1m3 <theprime@codingprime.com>
2012-03-10 09:28:25 -0600

    Add ie.css to asset precompilation

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,7 @@ module CodingPrime
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += %w(ie.css)
   end
 end
